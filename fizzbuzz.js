234907949394e9517aa18040cf825628e91b71d8
maddymakesgames <madelinemakesgames@gmail.com>
2019-11-16 19:07:53 -0500

    Added a start value and multiple players

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,9 +3,38 @@ const client = new (require('discord.js')).Client();
 client.on('message', (message) => {
 	if(message.content.startsWith('!fb') && !message.author.bot) {
 		let numStr = message.content.slice('!fb'.length).trim();
-		let num = parseInt(numStr);
-		let output = '';
-		for(let i = 0; i < num; i++) output += ((i.toString().includes('5') ? i.toString().match('5').join(' ').replace('5', 'Fizz ') + ' ': '') +  (i%5 == 0 ? 'Fizz ' : (i%7 == 0 ? 'Buzz' : i))).trim() + '\n';
+		let args = numStr.split(' ');
+		let players = 0;
+		let start = 0;
+		let end = 10;
+		switch(args.length) {
+			case 1:
+				end = args[0];
+				break;
+			case 2:
+				start = args[0];
+				end = args[1];
+				break;
+			case 3:
+			default:
+				players = args[0];
+				start = args[1];
+				end  = args[2];
+		}
+		let output = `__FizzBuzz from ${start} to ${end}${players > 0 ? ` with ${players} players` : ''}__\n`;
+		let currPlayer = 1;
+		let dir = false;
+		for(let i = start; i < end; i++) {
+			let currNumText = ((players > 0 ? `Player ${currPlayer}: ` : '') +(i.toString().includes('5') ? i.toString().match('5').join(' ').replace('5', 'Fizz ') + ' ': '') +  (i%5 == 0 ? 'Fizz ' : (i%7 == 0 ? 'Buzz' : i))).trim() + '\n';
+			output += currNumText;
+			if(players > 0){
+				if(currNumText.includes('Fizz') || currNumText.includes('Buzz')) currNumText.match(/Fizz|Buzz/g).forEach(() => dir = !dir);
+				if(dir) currPlayer--;
+				else currPlayer++;
+				if(currPlayer == 0) currPlayer = players;
+				if(currPlayer > players) currPlayer = 1;
+			}
+		}
 		safeSend(output, message.channel);
 	}
 });
